46e9cd1bc832e01ecaf822498f8e59619c91aff7
Gabriel <bielmelodossantos8@gmail.com>
2021-10-25 15:47:48 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.get('/filme', async(req, resp) => {
 
         a = a.map(item => {
             return {
+              id: item.id_filme,
               nome: item.nm_filme,
               genero: item.ds_genero,
               lancamento: item.ano_lancamento,
@@ -128,7 +129,88 @@ app.post('/filmesgosto', async(req, resp) => {
 })
 
 
-//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+app.get('/comentario', async(req, resp) => {
+    try {
+        let c = await db.infob_mw_comentario.findAll();
+
+        c = c.map(item => {
+            return {
+              id: item.id_cometario,
+              id_filme: item.id_filme,
+              id_usuario: item.id_usuario,
+              mensagem: item.ds_mensagem,
+              data: item.dt_comentario,
+              curtidas: item.ds_curtidas
+            }
+          })
+        resp.send(c);
+    } catch(e) {
+        resp.send({ erro: e.toString() })
+    }
+})
+
+
+app.post('/comentario', async(req, resp) => {
+    try {
+        let { id_filme, id_usuario, mensagem, curtidas } = req.body;
+        
+        let i = await db.infob_mw_comentario.create({
+            id_filme: id_filme,
+            id_usuario: id_usuario,
+            ds_mensagem: mensagem,
+            dt_comentario: new Date,
+            ds_curtidas: curtidas
+        })
+        resp.send("Comentario inserido!");
+    } catch(e) {
+        resp.send({erro: e.toString()})
+    }
+})
+
+
+app.put('/comentario/:id', async(req, resp) => {
+    try {
+        let { id_filme, id_usuario, mensagem,curtidas } = req.body;
+        let { id } = req.params;
+
+        let a = await db.infob_mw_comentario.update({
+            id_filme: id_filme,
+            id_usuario: id_usuario,
+            ds_mensagem: mensagem,
+            ds_curtidas: curtidas
+        },
+        {
+            where: {id_cometario: id}
+        })
+        resp.send("Comentario alterado!");
+    } catch(e) {
+        resp.send({erro: e.toString()})
+    }
+})
+
+
+app.delete('/comentario/:id', async(req, resp) => {
+    try {
+        let { id } = req.params;
+        let c = db.infob_mw_comentario.destroy({ where: {id_cometario: id}})
+        resp.send("Comentario removido!");
+    } catch(e) {
+        resp.send({ erro: e.toString()});
+    }
+})
+
+
+
+
+
+
+
+
+
 
 
 app.get('/usuario', async(req, resp) => {
@@ -283,69 +365,7 @@ app.post('/lista_item', async (req, resp) => {
 })
 
 
-app.get('/comentario', async(req, resp) => {
-    try {
-        let c = await db.infob_mw_comentario.findAll();
-        resp.send(c);
-    } catch(e) {
-        resp.send({ erro: e.toString() })
-    }
-})
 
-
-
-app.post('/comentario', async(req, resp) => {
-    try {
-        let { filme, usuario, mensagem, curtidas } = req.body;
-        
-        let i = await db.infob_mw_comentario.create({
-            id_filme: filme,
-            id_usuario: usuario,
-            ds_mensagem: mensagem,
-            dt_comentario: new Date,
-            ds_curtidas: curtidas
-        })
-        resp.send("Comentario inserido!");
-    } catch(e) {
-        resp.send({erro: e.toString()})
-    }
-})
-
-
-app.put('/comentario/:id', async(req, resp) => {
-    try {
-        let { filme, usuario, mensagem,curtidas } = req.body;
-        let { id } = req.params;
-
-        let a = await db.infob_mw_comentario.update({
-            id_filme: filme,
-            id_usuario: usuario,
-            ds_mensagem: mensagem,
-            ds_curtidas: curtidas
-        },
-        {
-            where: {id_cometario: id}
-        })
-        resp.send("Comentario alterado!");
-    } catch(e) {
-        resp.send({erro: e.toString()})
-    }
-})
-
-
-
-
-
-
-app.delete('/comentario/:id', async(req, resp) => {
-    try {
-        let { id } = req.params;
-        let c = db.infob_mw_comentario.destroy({ where: {id_cometario: id}})
-        resp.send("Comentario removido!");
-    } catch(e) {
-        resp.send({ erro: e.toString()});
-    }
-})
 
 
 
