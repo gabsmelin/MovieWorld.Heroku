6ff3b0b41ee6c90a7ff9c0786ec2c5904f39b0d9
alanfrei <ra37988720821@acaonsfatima.org.br>
2021-11-05 10:32:49 -0300

    asdasd

--- src/Controller/filmeController.js
+++ src/Controller/filmeController.js
@@ -30,7 +30,32 @@ app.get('/:id', async(req, resp) => {
 
 app.get('/', async(req, resp) => {
     try {
-        let a = await db.infob_mw_filmes.findAll({ });
+        let a = await db.infob_mw_filmes.findAll({limit:9});
+
+        a = a.map(item => {
+            return {
+              id: item.id_filme,
+              nome: item.nm_filme,
+              genero: item.ds_genero,
+              lancamento: item.ano_lancamento,
+              diretor: item.nm_diretor, 
+              sinopse: item.ds_sinopse,
+              avaliacao: item.ds_avaliacao, 
+              descricao: item.ds_descricao, 
+              plataforma: item.ds_plataforma, 
+              img_maior: item.img_capa_maior, 
+              img_menor: item.img_capa_menor
+            }
+          })
+        resp.send(a);
+    } catch(e) {
+        resp.send({erro: e.toString()})
+    }
+})
+
+app.get('/box', async(req, resp) => {
+    try {
+        let a = await db.infob_mw_filmes.findAll({limit:9});
 
         a = a.map(item => {
             return {
